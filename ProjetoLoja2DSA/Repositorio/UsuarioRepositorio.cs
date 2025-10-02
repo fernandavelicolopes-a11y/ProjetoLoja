99a985bb8fc21c1bca08dcd9700ec26f451e1d4b
fernanda <fernandavelicolopes@gmail.com>
2025-10-02 17:31:06 -0300

    iniciando a logica do usuario repositorio

--- ProjetoLoja2DSA/Repositorio/UsuarioRepositorio.cs
+++ ProjetoLoja2DSA/Repositorio/UsuarioRepositorio.cs
@@ -1,6 +1,17 @@
-﻿namespace ProjetoLoja2DSA.Repositorio
+﻿using ProjetoLoja2DSA.Models;
+
+namespace ProjetoLoja2DSA.Repositorio
 {
-    public class UsuarioRepositorio
+    using MySql.Data.MySqlClient;
+
+    public class UsuarioRepositorio(IConfiguration configuration)
     {
+        // Declara um campo privado somente leitura para armazenar a string de conexão com o MySQL.
+        private readonly string _conexaoMySQL = configuration.GetConnectionString("ConexaoMySQL");
     }
+}
+public Usuario ObterUsuario(string email)
+{
+
+
 }
