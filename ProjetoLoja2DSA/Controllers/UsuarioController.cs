331c7dd98a9a171e88a331219fb4a85b072ea2ad
fernanda <fernandavelicolopes@gmail.com>
2025-10-02 16:09:58 -0300

    Criando projeto

---
+++ ProjetoLoja2DSA/Controllers/UsuarioController.cs
@@ -0,0 +1,31 @@
+﻿//IMPORTA AS BIBLIOTECAS QUE SERÃO UTILIZADAS NO PROJETO
+using Microsoft.AspNetCore.Mvc;
+using ProjetoLoja2DSA.Repositorio;
+
+//DEFINE O NOME E ONDE A CLASSE USUARIOCONTROLLER ESTA LOCALIZADA
+// NAMESPACE AJUDA A ORGANIZAR O CODIGO E EVITAR CONFLITOS.
+namespace ProjetoLoja2DSA.Controllers
+{
+    //CLASSE USUARIOCONTROLLER QUE ESTA HERDANDO DA CLASSE PAI CONTROLLER
+    public class UsuarioController : Controller
+    {
+        //DECLARA A VARIAVEL PRIVADA E SOMENTE LEITURA DO TIPO USUARIOREPOSITORIO
+        //INSTANCIA O _usuarioController PARA SER ATRIBUIDO NO CONSTRUTOR E INTERAGIR COM O DADOS 
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        //DEFINE O CONRDTRUTOR DA CLASSE USUARIOCONTROLLER 
+        //RECEBE A INSTANCIA DE USUARIOREPOSITORIO  COM PARAMETROS
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
+      {
+            // O CONSTRUTOR É CHAMADO QUANDO UMA NOVA INSTANCIA É CRIADA
+            _usuarioRepositorio = usuarioRepositorio;
+      }
+
+
+        //INTERFACE QUE REPRESENTA O RESULTADO DA PÁGINA 
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ script.sql
@@ -0,0 +1,32 @@
+-- CRIANDO O BANCO DE DADOS 
+CREATE DATABASE bdloja2dsa;
+
+-- USANDO O BANCO DE DADOS
+USE bdloja2dsa;
+
+-- CRIANDO AS TABELAS DO BANCO DE DADOS
+CREATE TABLE Usuario(
+id int primary key auto_increment,
+email varchar(40) not null,
+senha varchar(40) not null
+);
+
+CREATE TABLE Cliente(
+codCli int primary key auto_increment,
+nomeCli varchar(40) not null,
+telCli varchar(40) not null,
+emailCli varchar(40) not null
+);
+-- CONSULTANDO AS TABELAS DO BANCO DE DADOS
+SELECT * FROM Usuario;
+SELECT * FROM Cliente;
+
+
+
+
+
+
+
+
+
+
